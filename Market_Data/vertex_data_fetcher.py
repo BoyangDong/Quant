482b8cd178b06c93f301fb9c64689f69331ad1a7
BoyangDong <bdong.bobo@gmail.com>
2017-12-15 15:52:54 -0600

    add sleep function to ensure process go through

--- Market_Data/vertex_data_fetcher.py
+++ Market_Data/vertex_data_fetcher.py
@@ -24,34 +24,50 @@ import urllib.request
 import json
 from JSONObject import JSONObject
 
+delay_in_sec = 3
+repeat_status_check = 6
+
 file_path = 'E:\\Repos\\Quant\\Market_Data\\'
 logf = open("E:\\Repos\\Quant\\Market_Data\\error_file.txt", "a")
 
+# url list of rest API calls
 for url in url_list:
+	time.sleep(delay_in_sec)
 	try:
 		with urllib.request.urlopen(url) as response:
-			while True:			
+			html = response.read()
+            ## Output formatted JSON data ## 
+			#parsed = json.loads(html)
+			#print (json.dumps(parsed, indent=4, sort_keys=True)) 
+			time.sleep(delay_in_sec)
+            #print(html)
+		count = 0
+		data = json.loads(html, object_hook=JSONObject)
+		status = data.RTickLead.fExtnDesc 
+		print(status)
+
+# check if file is ready to download else set up 10 second timer to wait for 
+		while status.lower() != 'ready' and count < repeat_status_check:
+			with urllib.request.urlopen(url) as response:
 				html = response.read()
-				## Output formatted JSON data ## 
-				#parsed = json.loads(html)
-				#print (json.dumps(parsed, indent=4, sort_keys=True)) 
-				data = json.loads(html, object_hook=JSONObject)
-				status = data.RTickLead.fExtnDesc 
-				print (status)
-				if status.lower() == 'ready':
-					download_url = data.RTickLead.fLeadPath
-					print (download_url)
-					product_name = data.RTickLead.fLeadSymb
-				#	download_url = r'%s'% ('http://Restv3.vertex-analytics.com:8080/V1/Data//ZNZ7_20171204/ZNZ7_20171204_10_OptRec_Events_20171204.zip')
-					urllib.request.urlretrieve(download_url, file_path + product_name + '_' + date + '.zip')
-					time.sleep(10)
-					break
-			time.sleep(5)
+				count += 1
+				time.sleep(delay_in_sec)
+				#print(html)
+			data = json.loads(html, object_hook=JSONObject)
+			status = data.RTickLead.fExtnDesc
+
+# when ready download product tick data
+		if status.lower() == 'ready':
+			download_url = data.RTickLead.fLeadPath
+			#find product symbol
+			product_name = data.RTickLead.fLeadSymb
+			print(product_name)
+			time.sleep(delay_in_sec)
+			urllib.request.urlretrieve(download_url, file_path + product_name + '_' + todays_date.strftime("%Y%m%d") + '.zip')
+			time.sleep(delay_in_sec)
 
 	except Exception as e:
 		logf.write("Failed to download {0}: {1}\n".format(str(url), str(e)))
-
-
 
 
 '''
